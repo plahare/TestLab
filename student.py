1e8417ba266278a2ba91e963d2f1ec110c7954b5
Priyanka Lahare <priyanka@Priyankas-MBP.hitronhub.home>
2018-04-22 10:01:19 -0700

    Adding my first class

---
+++ student.py
@@ -0,0 +1,10 @@
+class VGStudent:
+
+	 # set properties of any student through parameters
+	 def __init__(self, name, college):
+	 	self.name = name
+	 	self.college = college
+
+	 # this is a method because it belongs to class VGStudent
+	 def get_city(self):
+	 	return 'Mumbai'
